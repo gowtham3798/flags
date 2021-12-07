cf317bcffed2c7eb1852f544b2d53758467c5e06
raghul tamilarasan <gowtham3798@gmail.com>
2021-12-07 21:12:58 +0530

    quotes-api

--- script.js
+++ script.js
@@ -1,67 +1,47 @@
-const searchText = document.querySelector(".searchText");
-const searchBtn = document.querySelector(".btn");
-const displayResults = document.querySelector(".displayResult");
-const resultDiv = document.querySelector(".result");
-
-
- // reseting search box on every refresh
-document.addEventListener("DOMContentLoaded",function() {
-    searchText.value="";
-});
-
+ // getiing required elements using class name
+ const head = document.querySelector(".head");
+ const displayResults = document.querySelector(".displayResult");
  
-//function to be performed during the search
-searchBtn.addEventListener("click", function () {
-    if(searchText.value===""){
-        resultDiv.hidden=true;
-        alert("please come up with any letters to search");
-    }
-    else if(searchText.value!=""){
- const ApiURL = "https://api.jikan.moe/v3/search/anime?q="+`${searchText.value}`;
 
 
- //nested-function for every individual results
- function renderResult(result) {
-  const resultDiv = document.createElement("div");
-  resultDiv.className = "card";
-  resultDiv.innerHTML = `
-    <h2>${result.title}</h2>
-    <img src="${result.image_url}" class="result-photo" />
-    <p>Synopsis - ${result.synopsis}</p>
-    <p>Episodes - ${result.episodes}</p>
-    <p>Score - ${result.score}</p>
-    <p>Rated - ${result.rated}</p>
-    `;
-  displayResults.appendChild(resultDiv);
-}
+  // reseting search box on every refresh
+ document.addEventListener("DOMContentLoaded",function() {
+   
+const ApiURL = "https://quote-garden.herokuapp.com/api/v3/quotes";
 
-//function to get all results (displays max 20 results)
-function renderAll(results) {
-  displayResults.innerHTML = "";
-  resultDiv.hidden = false;
-  let count=0;
-  for(let result of results){
-      if(result.rated==="PG-13"){ 
-        renderResult(result);
-         count++;
-       if(count===20)break;
-      }
-  }
-}
 
-  //fetching the results based on searchText
-let func = async function(ApiURL){
-    try{
-    const response = await fetch(ApiURL);
-    var result = await response.json();
-    renderAll(result.results);
-    }
-    catch(error){
-      console.log(error.message);
-      alert("try to be more specific")
-    }
-}
-// since fetching is under anonymous function , calling the function    
-func(ApiURL);
-}
-});
+  //nested-function for every individual results
+  function renderResult(result) {
+   const resultDiv = document.createElement("div");
+   resultDiv.className = "card";
+   resultDiv.innerHTML = `
+     <p>${result.quoteText}</p>
+     <h2>"${result.quoteAuthor}"</h2>
+     `;
+   displayResults.appendChild(resultDiv);
+ }
+
+ //function to get all results (displays max 20 results)
+ function renderAll(results) {
+   displayResults.innerHTML = "";
+   for(let result of results){ 
+         renderResult(result);
+       }
+   }
+ 
+   //fetching the results based on searchText
+ let func = async function(ApiURL){
+     try{
+     const response = await fetch(ApiURL);
+     var results = await response.json();
+     console.log(results);
+     renderAll(results.data);
+     }
+     catch(error){
+       console.log(error.message);
+       alert("sorry try again one more time friend!!!")
+     }
+ }
+ // since fetching is under anonymous function , calling the function    
+ func(ApiURL);
+ });
